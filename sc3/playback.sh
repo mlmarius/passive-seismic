df2dc7793f423be66a1ca1c2ebeab3152fd8d7f8
Sudipta Basak <basaks@gmail.com>
2017-08-23 21:21:58 +1000

    xml playback also need spread and scmaster

--- sc3/playback.sh
+++ sc3/playback.sh
@@ -4,7 +4,7 @@ if [ "$#"  -lt 1 ]; then
 echo "Usage: $0 [mseed-volume]";
 exit 0;
 fi
-set -x; seiscomp stop
+set -x; seiscomp stop; seiscomp start spread scmaster
 DBFLAG="mysql://sysop:sysop@localhost/seiscomp3"
 VERBOSITY="-v"
 
@@ -14,4 +14,4 @@ scamp -ep origins.xml -I file://$1 -d $DBFLAG $VERBOSITY > amps.xml
 scmag -ep amps.xml -d $DBFLAG $VERBOSITY > mags.xml
 scevent -ep mags.xml -d $DBFLAG $VERBOSITY > events.xml
 
-scdb -i events.xml -d $DBFLAG $VERBOSITY
+# scdb -i events.xml -d $DBFLAG $VERBOSITY
