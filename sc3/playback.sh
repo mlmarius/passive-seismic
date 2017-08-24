0559f78ec5e63bbf39d1a71ddd9a1a9e54bd584e
Sudipta Basak <basaks@gmail.com>
2017-08-24 19:41:16 +1000

    need `--` before `ep`

--- sc3/playback.sh
+++ sc3/playback.sh
@@ -8,10 +8,10 @@ set -x; seiscomp stop; seiscomp start spread scmaster
 DBFLAG="mysql://sysop:sysop@localhost/seiscomp3"
 VERBOSITY="-v"
 
-scautopick -ep --playback -I file://$1 -d $DBFLAG > picks.xml
-scautoloc -ep picks.xml -d $DBFLAG $VERBOSITY > origins.xml
-scamp -ep origins.xml -I file://$1 -d $DBFLAG $VERBOSITY > amps.xml
-scmag -ep amps.xml -d $DBFLAG $VERBOSITY > mags.xml
-scevent -ep mags.xml -d $DBFLAG $VERBOSITY > events.xml
+scautopick --ep --playback -I file://$1 -d $DBFLAG > picks.xml
+scautoloc --ep picks.xml -d $DBFLAG $VERBOSITY > origins.xml
+scamp --ep origins.xml -I file://$1 -d $DBFLAG $VERBOSITY > amps.xml
+scmag --ep amps.xml -d $DBFLAG $VERBOSITY > mags.xml
+scevent --ep mags.xml -d $DBFLAG $VERBOSITY > events.xml
 
 # scdb -i events.xml -d $DBFLAG $VERBOSITY
