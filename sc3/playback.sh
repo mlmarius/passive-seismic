4ac2de1e950738365bb11572fb93f85282438d87
Sudipta Basak <basaks@gmail.com>
2017-08-26 10:26:00 +1000

    need to use l1 versions of pick/loc

--- sc3/playback.sh
+++ sc3/playback.sh
@@ -8,8 +8,8 @@ set -x; seiscomp stop; seiscomp start spread scmaster
 DBFLAG="mysql://sysop:sysop@localhost/seiscomp3"
 VERBOSITY="-v"
 
-scautopick --ep --playback -I file://$1 -d $DBFLAG > picks.xml
-scautoloc --ep picks.xml -d $DBFLAG $VERBOSITY > origins.xml
+l1autopick --ep --playback -I file://$1 -d $DBFLAG > picks.xml
+l1autoloc --ep picks.xml -d $DBFLAG $VERBOSITY > origins.xml
 scamp --ep origins.xml -I file://$1 -d $DBFLAG $VERBOSITY > amps.xml
 scmag --ep amps.xml -d $DBFLAG $VERBOSITY > mags.xml
 scevent --ep mags.xml -d $DBFLAG $VERBOSITY > events.xml
