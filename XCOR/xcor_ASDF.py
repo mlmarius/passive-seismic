212869fdf74c82319ca54fd08f3dcbaead4095c6
outdoorpet <acooper8@utas.edu.au>
2017-10-17 19:40:20 +1100

    continued writing so that netcdf files are produced

--- XCOR/xcor_ASDF.py
+++ XCOR/xcor_ASDF.py
@@ -5,7 +5,9 @@ from xcorqc.xcorqc import xcorr2
 from obspy import Stream, Trace
 import matplotlib.pyplot as plt
 from obspy import UTCDateTime
-
+from collections import defaultdict
+from netCDF4 import Dataset
+import numpy as np
 
 code_start_time = time.time()
 
@@ -13,16 +15,25 @@ code_start_time = time.time()
 # =========================== User Input Required =========================== #
 
 #Path to the ASDF file for xcor
-data_path = '/g/data1/ha3/Passive/_AusArray/OA/processing/test_xcor_ashby/two_stns_xcor_test.h5'
-out = '/g/data1/ha3/Passive/_AusArray/OA/processing/test_xcor_ashby/two_stns_xcor_out.h5'
+data_path = '/g/data1/ha3/Passive/_AusArray/OA/processing/test_xcor_ashby/one_stn_xcor_test.h5'
+asdf_out = '/g/data1/ha3/Passive/_AusArray/OA/processing/test_xcor_ashby/one_stn_xcor_out.h5'
 
 temp_net = "OA"
+
+# Interval over which windows are stacked
+intervalSeconds     = 3600*24 # 1 day
+# Size of data-window to be stacked
+windowSeconds       = 3600
+
+# == Output Path ==
+outputDir           = '/g/data1/ha3/Passive/_AusArray/OA/processing/test_xcor_ashby/'
+
 
 # =========================================================================== #
 
 
-if os.path.exists(out):
-    os.remove(out)
+if os.path.exists(asdf_out):
+    os.remove(asdf_out)
 
 ds = pyasdf.ASDFDataSet(data_path)
 print(ds.waveforms.list())
@@ -56,28 +67,21 @@ for net_sta in ds.waveforms.list():
 
 print('')
 
+
+# function to make a number into a 3 digit string with leading zeros
+def make_threedig(a):
+    if len(a) == 1:
+        return '00' + a
+    elif len(a) == 2:
+        return '0' + a
+    return a
+
+
+
+
 def xcor_process(st, inv):
 
     xcor_st = Stream()
-
-    # tr1 = Stream([st[0]])
-    # tr2 = Stream([st[1]])
-    #
-    # y, x, comp = IntervalStackXCorr(tr1, tr2)
-    #
-    # for day_stack_xcor in y:
-    #     print(type(day_stack_xcor))
-    #     print(day_stack_xcor[0])
-    #
-    #     # fill in headers
-    #     stats = {'network': tr1[0].stats.network, 'station': tr1[0].stats.station, 'location': '',
-    #              'channel': tr1[0].stats.channel, 'npts': len(day_stack_xcor[0]), 'sampling_rate': 100,
-    #              'mseed': {'dataquality': 'D'}}
-    #
-    #     stats["starttime"] = tr1[0].stats.starttime
-    #
-    #     xcor_st += Trace(data=day_stack_xcor[0], header=stats)
-
 
 
     for tr in st:
@@ -87,6 +91,9 @@ def xcor_process(st, inv):
 
         # get the uid label
         uid_label = tr.stats.asdf.labels[1]
+
+        # get the region name
+        region_id = tr.stats.asdf.labels[0]
 
         print(uid_label)
 
@@ -102,7 +109,7 @@ def xcor_process(st, inv):
         print(perm_st)
         print("DO XCOR......")
 
-        xcl, winsPerInterval = xcorr2(ref_tr, temp_tr)
+        xcl, winsPerInterval = xcorr2(ref_tr, temp_tr, interval_seconds=intervalSeconds, window_seconds=windowSeconds)
 
         if (xcl is None):
             print("\t\tWarning: no cross-correlation results returned for station-pair %s, " %
@@ -110,50 +117,81 @@ def xcor_process(st, inv):
             continue
 
 
+
+
         print(xcl)
+        print(xcl.shape)
         print(winsPerInterval)
-
-        # saveXCorrPlot(y, x, '/g/data/ha3/', 'test_plot', comp)
-
-
-        # fill in headers for new xcor trace
-        stats = {'network': tr.stats.network, 'station': tr.stats.station, 'location': "",
-                 'channel': uid_label[2:], 'npts': len(xcl[0]), 'sampling_rate': tr.stats.sampling_rate,
-                 'mseed': {'dataquality': 'D'}, "asdf": {}}
+        print(winsPerInterval.shape)
 
 
-        stats["starttime"] = tr.stats.starttime
+        for _i, day_xcor in enumerate(xcl):
+            print(day_xcor)
 
-        temp_tr = Trace(data=xcl[0], header=stats)
+            # fill in headers for new xcor trace
+            stats = {'network': tr.stats.network, 'station': tr.stats.station, 'location': "",
+                     'channel': make_threedig(str(_i)), 'npts': len(xcl[0]), 'sampling_rate': tr.stats.sampling_rate,
+                     'mseed': {'dataquality': 'D'}, "asdf": {}}
 
-        temp_tr.stats.asdf.labels = tr.stats.asdf.labels
 
-        xcor_st += temp_tr
+            stats["starttime"] = tr.stats.starttime
 
+            temp_tr = Trace(data=day_xcor, header=stats)
 
-        # break
+            added_labels = [region_id, stationPair, str(winsPerInterval[_i])]
 
+            print(added_labels)
+
+            temp_tr.stats.asdf.labels = added_labels
+
+            xcor_st += temp_tr
 
     return xcor_st
 
 
 
 ds.process(process_function=xcor_process,
-           output_filename=out,
+           output_filename=asdf_out,
            tag_map={"raw_recording": "xcor"})
 
 
-# load in new ASDF that has been written
-new_ds = pyasdf.ASDFDataSet(out)
 
-# now just copy over waveforms from input ASDF file to new ASDF
-for sta_id in ds.waveforms.list():
-    sta_acc = ds.waveforms[sta_id]
-    for asdf_st_id in sta_acc.list():
-        if not asdf_st_id == "StationXML":
-            new_ds.add_waveforms(sta_acc[asdf_st_id],tag=asdf_st_id.split('__')[3])
-        else:
-            new_ds.add_stationxml(sta_acc[asdf_st_id])
+tmp_results_dict = defaultdict(list)  # Results dictionary indexed by station-pair string
+tmp_windows_count_dict = defaultdict(list)  # Window-count dictionary indexed by station-pair string
+
+# load in new ASDF that has been written
+new_ds = pyasdf.ASDFDataSet(asdf_out)
+
+# extract all of the xcor
+net_sta_list = new_ds.waveforms.list()
+
+for net_sta in net_sta_list:
+    sta_accessor = new_ds.waveforms[net_sta]
+
+    tags_list = sta_accessor.list()
+
+
+
+    for wave_id in tags_list:
+        if wave_id == "StationXML":
+            # ignore the station xml file
+            continue
+
+        #open up the trace
+        tmp_st = sta_accessor[wave_id]
+        tmp_tr = tmp_st[0]
+
+        #get labels
+        tmp_labels = tmp_tr.stats.asdf.labels
+
+        print(tmp_labels)
+
+        # make new key
+        window_key = tmp_labels[1]+'_'+tmp_labels[0]
+
+        tmp_results_dict[window_key].append(tmp_tr.data)
+        tmp_windows_count_dict[window_key].append(int(tmp_labels[2]))
+
 
 
 
@@ -161,3 +199,85 @@ for sta_id in ds.waveforms.list():
 del new_ds
 
 del ds
+
+print(tmp_results_dict)
+print(tmp_windows_count_dict)
+
+
+
+print('=== Collating Results ===')
+x = None
+# Concatenate results
+for k in tmp_results_dict.keys():
+    combinedXcorrResults = None
+    combinedWindowCountResults = None
+
+    print(k)
+
+
+    print(len(tmp_results_dict[k]))
+
+    for i in np.arange(len(tmp_results_dict[k])):
+        print(i)
+        if (i == 0):
+            combinedXcorrResults = np.array([tmp_results_dict[k][0]])
+            combinedWindowCountResults = np.array([tmp_windows_count_dict[k][0]])
+
+            print(combinedXcorrResults.shape)
+
+            # Generate time samples (only needs to be done once)
+            if (x is None):
+                x = np.linspace(-windowSeconds, windowSeconds,
+                                len(tmp_results_dict[k][0]))
+                # end if
+        else:
+            combinedXcorrResults = np.concatenate((combinedXcorrResults,
+                                                   np.array([tmp_results_dict[k][i]])), axis=0)
+
+            combinedWindowCountResults = np.concatenate((combinedWindowCountResults,
+                                                         np.array([
+                                                             tmp_windows_count_dict[k][i]])), axis=0)
+            # end if
+    # end for
+
+    # Replace lists with combined results
+    tmp_results_dict[k] = combinedXcorrResults
+    tmp_windows_count_dict[k] = combinedWindowCountResults
+
+    print(combinedXcorrResults)
+    print(combinedXcorrResults.shape)
+    print(combinedWindowCountResults)
+    print(combinedWindowCountResults.shape)
+# end for
+
+
+
+print(tmp_results_dict)
+print(tmp_windows_count_dict)
+
+
+# Save Results
+for i, k in enumerate(tmp_results_dict.keys()):
+    fn = os.path.join(outputDir, '%s.nc'%(k))
+    print('\t Writing: %s'%(fn))
+
+    root_grp = Dataset(fn, 'w', format='NETCDF4')
+    root_grp.description = 'Cross-correlation results for station-pair: %s' % (k)
+
+    print(tmp_results_dict[k].shape)
+    # Dimensions
+    root_grp.createDimension('time', tmp_results_dict[k].shape[0])
+    root_grp.createDimension('lag', tmp_results_dict[k].shape[1])
+
+    # Variables
+    time = root_grp.createVariable('time', 'f4', ('time',))
+    lag = root_grp.createVariable('lag', 'f4', ('lag',))
+    nsw = root_grp.createVariable('NumStackedWindows', 'f4', ('time',))
+    xc = root_grp.createVariable('xcorr', 'f4', ('time', 'lag',))
+
+    # Populate variables
+    time[:] = np.arange(tmp_results_dict[k].shape[0])
+    lag[:] = x
+    nsw[:] = tmp_windows_count_dict[k]
+    xc[:, :] = tmp_results_dict[k][::-1, :]  # Flipping rows
+    root_grp.close()
